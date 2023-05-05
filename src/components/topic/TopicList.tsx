61c54a5069e4dba0ff125f85b6274df3c1efe337
Baptiste Goulard <baptiste.goulard@jetdev.fr>
2023-05-05 14:42:47 +0200

    :sparkles: create basic topic components

---
+++ src/components/topic/TopicList.tsx
@@ -0,0 +1,30 @@
+import { Topic } from '@prisma/client'
+import React from 'react'
+
+type TopicListProps = {
+    topics: Topic[]
+}
+
+const TopicList = ({ topics }: TopicListProps) => {
+    return (
+        <div className="flex flex-col my-5">
+            <div className='grid grid-cols-4 gap-2 font-bold mb-2'>
+                <div>Title</div>
+                <div>Description</div>
+                <div>StartDate</div>
+                <div>EndDate</div>
+            </div>
+            {topics.map(topic => (
+                <div className='grid grid-cols-4 gap-2 border-t py-2' key={topic.id}>
+                    <div>{topic.title}</div>
+                    <div>{topic.description}</div>
+                    <div>{topic.startDate.toDateString()}</div>
+                    <div>{topic.endDate.toDateString()}</div>
+                </div>
+            )
+            )}
+        </div>
+    )
+}
+
+export default TopicList
